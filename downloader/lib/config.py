eacf6f815e46fcdfe56aac8bbf5af0f9ee50b0f5
Devon <devon.hjelm@microsoft.com>
2020-04-02 21:07:34 -0700

    moving stuff

--- downloader/lib/config.py
+++ downloader/lib/config.py
@@ -1,7 +1,7 @@
 import os
 from pathlib import Path
 
-DATASET_ROOT = "/media/gitumarkk/Seagate Backup Plus Drive//Dancelogue/DATASETS/Kinetics/"
+DATASET_ROOT = "/mnt/blobfusetmp/kinetics600/"
 TRAIN_ROOT = os.path.join(DATASET_ROOT, "train")
 VALID_ROOT = os.path.join(DATASET_ROOT, "val")
 TEST_ROOT = os.path.join(DATASET_ROOT, "test")
@@ -17,8 +17,8 @@ TEST_SOUND_ROOT = os.path.join(DATASET_ROOT, "test_sound")
 CATEGORIES_PATH = "resources/categories.json"
 CLASSES_PATH = "resources/classes.json"
 
-TRAIN_METADATA_PATH = "resources/700/train/kinetics_700_train.json"
-VAL_METADATA_PATH = "resources/700/val/kinetics_700_val.json"
-TEST_METADATA_PATH = "resources/700/test/kinetics_700_test.json"
+TRAIN_METADATA_PATH = "resources/600/kinetics_600_train.json"
+VAL_METADATA_PATH = "resources/600/kinetics_600_val.json"
+TEST_METADATA_PATH = "resources/600/kinetics_600_test.json"
 
 SUB_CLASS_PATH = "resources/700/categories.json"
